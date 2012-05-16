8f678e07880e90dbf6d4068fb9ddc9fa4c8b08d6
Eli Ribble <eribble@hirevue.com>
2012-05-16 16:53:44 -0600

    Added DecisionActions which can be used to signal decisions when a decision task is completed. Fixed up the documentation on the data coming back from polling for a decision task

--- boto/swf/decision.py
+++ boto/swf/decision.py
@@ -1,8 +1,65 @@
+import simplejson
+
+class DecisionAction():
+    """Some sort of action taken when a decision is finished"""
+    pass
+
+class ScheduleActivityTask(DecisionAction):
+    """A decision to schedule a new activity"""
+
+    def __init__(self, decision_task, activity_type_name, activity_type_version, activity_id, activity_input=''):
+        self.decision_task          = decision_task
+        self.activity_type_name     = activity_type_name
+        self.activity_type_version  = activity_type_version
+        self.activity_id            = activity_id
+        self.activity_input         = activity_input
+ 
+    def __str__(self):
+        return simplejson.dumps(self.data)
+
+    @property
+    def data(self):
+        return {
+            'decisionType'                          : 'ScheduleActivityTask',
+            'scheduleActivityTaskDecisionAttributes': {
+                'activityId'                        : self.activity_id,
+                'activityType'                      : {
+                    'name'                          : self.activity_type_name,
+                    'version'                       : self.activity_type_version,
+                },
+                'input'                                 : self.activity_input,
+             },
+        }
+
 class DecisionTask():
     """
     Example data for a decision task:
 
-{'previousStartedEventId': 0, 'workflowExecution': {'workflowId': 'thumbnail@-eli-test3', 'runId': 'e56e9b8e-788c-428f-9824-2e8cba7a0cf2'}, 'startedEventId': 3, 'workflowType': {'version': '1.2', 'name': 'Thumbnail'}, 'events': [{'eventId': 1, 'eventType': 'WorkflowExecutionStarted', 'workflowExecutionStartedEventAttributes': {'taskList': {'name': 'ThumbnailTaskList'}, 'parentInitiatedEventId': 0, 'taskStartToCloseTimeout': '3600', 'childPolicy': 'TERMINATE', 'executionStartToCloseTimeout': '3600', 'workflowType': {'version': '1.2', 'name': 'Thumbnail'}}, 'eventTimestamp': 1337186157.838}, {'eventId': 2, 'eventType': 'DecisionTaskScheduled', 'decisionTaskScheduledEventAttributes': {'startToCloseTimeout': '3600', 'taskList': {'name': 'ThumbnailTaskList'}}, 'eventTimestamp': 1337186157.838}, {'eventId': 3, 'decisionTaskStartedEventAttributes': {'scheduledEventId': 2}, 'eventTimestamp': 1337187977.553, 'eventType': 'DecisionTaskStarted'}], 'taskToken': 'AAAAKgAAAAEAAAAAAAAAAeIiGiJNausk3GXpBMvNTeO8+FNwFRCOG5SY+630ARlworccaWRplGwTMcyqTKK7JDyFaeH0+82VxBx/CIETMaYrT7KBiUjNnEBpLBMNRhnUY1AN1c0eSL0WHAxvAXJG4UTAkUB1NN0gMLAdPWq/AMU3EDGE7g1niR6TAMbu4FIOZoF7ekvnbXrtbkj4SsbE8BLEHcxwlRyIP19OszD0p3WcqY4F2DLcH27bl46Z9plj9tW6N851t++e8o2ezCJa/1jBo935IISDqx4965Lz9kU='}
+{'events': [{'eventId': 1,
+             'eventTimestamp': 1337206241.18,
+             'eventType': 'WorkflowExecutionStarted',
+             'workflowExecutionStartedEventAttributes': {'childPolicy': 'TERMINATE',
+                                                         'executionStartToCloseTimeout': '3600',
+                                                         'parentInitiatedEventId': 0,
+                                                         'taskList': {'name': 'ThumbnailTaskList'},
+                                                         'taskStartToCloseTimeout': '3600',
+                                                         'workflowType': {'name': 'Thumbnail',
+                                                                          'version': '1.2'}}},
+            {'decisionTaskScheduledEventAttributes': {'startToCloseTimeout': '3600',
+                                                      'taskList': {'name': 'ThumbnailTaskList'}},
+             'eventId': 2,
+             'eventTimestamp': 1337206241.18,
+             'eventType': 'DecisionTaskScheduled'},
+            {'decisionTaskStartedEventAttributes': {'scheduledEventId': 2},
+             'eventId': 3,
+             'eventTimestamp': 1337206241.391,
+             'eventType': 'DecisionTaskStarted'}],
+ 'previousStartedEventId': 0,
+ 'startedEventId': 3,
+ 'taskToken': 'AAAAKgAAAAEAAAAAAAAAAeFU+YjFj6pKW4kYYI0e07mfx0jH1O8BLuY1WsNevwKLhHnzkuAKgf90PVmt6FEU1YZsOgVKPNcXk9aQGAIyh/OOVfKaxETdSofzrJJbTbo3Hs2T1tuWUTY3weX9ZeqQZKZqfcDHOlOISiwI74PVUcQa2iRlmHW+DX3cOEFA/t1h6whDwbWkflJ7R2sCrTS2kpro57H57kQBj/1vZn8F9FiluAeC18ujcWfeUUOmFxbr6qJHOzTBVnzh0Iypxeg5xcerR5q+tdnlTrdLkHVsKlo=',
+ 'workflowExecution': {'runId': 'e1552210-241a-42ec-b87a-eb42e19d7533',
+                       'workflowId': 'thumbnail@test13'},
+ 'workflowType': {'name': 'Thumbnail', 'version': '1.2'}}
     """
     def __init__(self, task_list, data):
         self.task_list = task_list
@@ -15,4 +72,15 @@ class DecisionTask():
     @property
     def run_id(self):
         return self._data['workflowExecution']['runId']
+ 
+    @property
+    def task_token(self):
+        return self._data['taskToken']
+
+    def finish( self, decisions ):
+        """Finish this decision task and signal the provided decisions"""
+        import pdb;pdb.set_trace()
+        self.task_list.finish_decision( self, decisions )
         
+    def ScheduleActivityTask( self, activity_type_name, activity_type_version, activity_id, activity_input='' ):
+        return ScheduleActivityTask( self, activity_type_name, activity_type_version, activity_id, activity_input )
--- boto/swf/task_list.py
+++ boto/swf/task_list.py
@@ -1,3 +1,5 @@
+from boto.swf.decision import DecisionTask
+
 class TaskList():
     def __init__(self, domain, name):
         self.swf = domain.swf
@@ -6,4 +8,12 @@ class TaskList():
 
     def get_next_decision(self):
         """Long polling call that will get the next decision or eventually time out and return None"""
-        return self.swf.poll_for_decision_task( self.domain_name, self.name )
+        decision_data = self.swf.poll_for_decision_task( self.domain_name, self.name )
+        if 'taskToken' not in decision_data: #decision_data['startedEventId'] == 0 and decision_data['previousStartedEventId'] == 0:
+            return None
+        return DecisionTask(self, decision_data)
+
+    def finish_decision(self, decision_task, decisions):
+        """Finish the provided decision task by signaling the provided decisions"""
+        decision_text = [d.data for d in decisions] 
+        self.swf.respond_decision_task_completed( decision_task.task_token, decision_text )
